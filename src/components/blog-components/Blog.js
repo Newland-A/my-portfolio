4fc55fd7792b7fbb2ac1bb1eb03b9edc86415931
Newland-A <ajn252@gmail.com>
2021-01-13 09:08:11 -0500

    delete recreate rename folder for blog components

---
+++ src/components/blog-components/Blog.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import CommentContainer from '../../containers/CommentContainer';
+
+export default class Blog extends Component {
+  state = {
+    postId: parseInt(document.location.href.match(/\d+/g)),
+    post: {}
+  }
+
+  componentDidMount(){
+    fetch(`http://192.168.1.11:3001/posts/${this.state.postId}`)
+    .then(resp => resp.json())
+    .then(post => this.setState({
+      ...this.state,
+      post: post
+    }))
+  }
+
+  componentDidUpdate(){
+    document.getElementById('blog-title').innerHTML = this.state.post.title
+    document.getElementById('blog-body').innerHTML = this.state.post.content
+  }
+
+  render() {
+    return (
+      <div className="blog-container">
+        <div className="blog-content">
+          <h2 id="blog-title">Loading Please Wait...</h2>
+          <div id="blog-body"> </div>
+        </div>
+        <CommentContainer postId={this.state.postId} />
+      </div>
+    );
+  }
+}
+
