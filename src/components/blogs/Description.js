f2004c0204156605f0ab3ab3a5c95da7796d3fa7
Newland-A <ajn252@gmail.com>
2021-01-13 08:52:39 -0500

    removed blog components from folder

---
+++ src/components/blogs/Description.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Description extends Component {
+  render() {
+    return (
+      <div>
+        <p>Description about the blog</p>
+      </div>
+    );
+  }
+}
+
+export default Description;
---
+++ src/components/blogs/Title.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Title extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Blogs</h2>
+      </div>
+    );
+  }
+}
+
+export default Title;
