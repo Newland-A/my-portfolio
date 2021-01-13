bff77bba8029fe6885ec3559a34532821a85c807
Newland-A <ajn252@gmail.com>
2021-01-13 09:09:55 -0500

    create folder and files for heading of page

---
+++ src/components/header/AboutMe.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AboutMe extends Component {
+  render() {
+    return (
+      <div>
+        <p className="subtitle">I am a full stack software engineer, who enjoys solving problems and designing a visually pleasing web application.</p>
+      </div>
+    );
+  }
+}
+
+export default AboutMe;
---
+++ src/components/header/Myname.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Myname extends Component {
+  render() {
+    return (
+      <div>
+      {/* Change this to something */}
+        <h1>My name is <strong>Amy Newland</strong></h1>
+      </div>
+    );
+  }
+}
+
+export default Myname;
