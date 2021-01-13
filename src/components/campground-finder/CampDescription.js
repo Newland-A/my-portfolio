d531d6b667ffbc2a1b5facfd7761b0a1fd044f83
Newland-A <ajn252@gmail.com>
2021-01-13 09:09:02 -0500

    add campground placeholder files

---
+++ src/components/campground-finder/CampDescription.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CampDescription extends Component {
+  render() {
+    return (
+      <div>
+        <p>This is how I spend my spare time. Maybe you would like to visit and leave a review and next time I have some spare time. I'll check it out.</p>
+      </div>
+    );
+  }
+}
+
+export default CampDescription;
