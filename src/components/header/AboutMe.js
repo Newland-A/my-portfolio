328ab3a800e57e7c15efe92e54fd1302a40d11b4
Newland-A <ajn252@gmail.com>
2021-01-15 14:44:03 -0500

    change wording of my description

--- src/components/header/AboutMe.js
+++ src/components/header/AboutMe.js
@@ -4,7 +4,9 @@ class AboutMe extends Component {
   render() {
     return (
       <div>
-        <p className="subtitle">I am a full stack software engineer, who enjoys solving problems and designing a visually pleasing web application.</p>
+        <p className="subtitle">
+          Engineer experienced in Ruby on Rails and JavaScript / ReactJS based programming and a background in graphic design and leadership. Being a Stay-at-HomeSchool mom has given me considerable experience in problem-solving, leadership and making research-driven decisions. Being a mom has taught me with determination and persistence, anything can be accomplished. With me, you will be gaining a highly self-motivated and team-oriented person. I look forward to making connections and growing my knowledge with every new challenge.
+        </p>
       </div>
     );
   }
