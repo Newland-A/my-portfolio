07ca4999dce8c9e92e31fe51005cde74c0af3597
Newland-A <ajn252@gmail.com>
2021-01-20 06:37:18 -0500

    create main Campground component using a functional component with hooks

--- src/components/campground-finder/Campground.js
+++ src/components/campground-finder/Campground.js
@@ -1,22 +1,34 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import ReviewContainer from '../../containers/ReviewContainer.js'
 
-class Campground extends Component {
-  state = {
-    postid: parseInt(document.location.href.match(/\d+/g)),
-    campgrounds: {}
+const Campground = ({ match }) => {
+  const { campgroundId } = match.params
+
+  const campground = useSelector(state => state.campgrounds.find(campground => campground.id === campgroundId))
+
+  useEffect(() => {
+    document.name = campground.name;
+    document.description = campground.description;
+  })
+
+  if(!campground) {
+    return(
+      <section>
+        <p>That campground is not here yet! Please check back at a later time!</p>
+      </section>
+    )
   }
 
-  componentDidMount() {
-    debugger
-    fetch(local)
-  }
-  render() {
-    return (
-      <div>
-        
+  return (
+    <section>
+      <div className="campground">
+        <h2 id="camp-name">{campground.name}</h2>
+        <p className="camp-description">{campground.description}</p>
       </div>
-    );
-  }
+      <ReviewContainer campgroundId={this.state.campgroundId} />
+    </section>
+  )
 }
 
 export default Campground;
