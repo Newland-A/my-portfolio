730f73441fc1b70f878a9fdf03f4651aca510f68
Newland-A <ajn252@gmail.com>
2021-01-20 14:48:21 -0500

    change naming of case statements

--- src/redux/actions/campgroundActions.js
+++ src/redux/actions/campgroundActions.js
@@ -1,18 +1,19 @@
 export const fetchCampgrounds = () => {
-  return dispatch => { 
-    dispatch({type: 'FETCHING_CAMPGROUNDS'})
-  fetch('http://localhost:3000/campgrounds')
-  .then(resp => resp.json())
-  .then(campgrounds => {
-    dispatch({
-      type: 'SOURCE_CAMPGROUNDS', 
-      payload: campgrounds.sort((a, b) => {
-        let aScore = a.avg_score
-        let bScore = b.avg_score
-
-        return aScore - bScore
+  return dispatch => {
+    dispatch({type: 'FETCHING_CAMPS'})
+    // 
+    fetch('http://localhost:3000/campgrounds')
+    .then(resp => resp.json())
+    .then(campgrounds => {
+      dispatch({
+        type: 'SOURCE_CAMPS',
+        payload: campgrounds.sort((a, b) => {
+          let aDate = new Date(a.created_at)
+          let bDate = new Date(b.created_at)
+          return bDate - aDate
+        })
       })
     })
-  })
-}
+    // 
+  }
 }
