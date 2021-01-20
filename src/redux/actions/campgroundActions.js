00a3c1127625e8f9284a1c034aa601a42e1a1d1b
Newland-A <ajn252@gmail.com>
2021-01-20 06:49:08 -0500

    copy over and change blog action to campground action

--- src/redux/actions/campgroundActions.js
+++ src/redux/actions/campgroundActions.js
@@ -1,14 +1,18 @@
 export const fetchCampgrounds = () => {
   return dispatch => { 
-    dispatch({type: 'FETCHING_POSTS'})
+    dispatch({type: 'FETCHING_CAMPGROUNDS'})
   fetch('http://localhost:3000/campgrounds')
   .then(resp => resp.json())
-  .then(posts => {
+  .then(campgrounds => {
     dispatch({
-      type: 'SOURCE_POSTS', 
-      payload: posts.sort((camp) => {
-        
+      type: 'SOURCE_CAMPGROUNDS', 
+      payload: campgrounds.sort((a, b) => {
+        let aScore = a.avg_score
+        let bScore = b.avg_score
+
+        return aScore - bScore
       })
     })
   })
+}
 }
