61ccd8b8905ced0f3c753b53aedc7c118ec19d3b
Newland-A <ajn252@gmail.com>
2021-01-20 14:49:10 -0500

    import campground reducer into the combined reducers

--- src/redux/index.js
+++ src/redux/index.js
@@ -2,9 +2,11 @@ import { combineReducers, createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import blog from './reducers/blogReducer';
+import camp from './reducers/campgroundReducer';
 
 const rootReducer = combineReducers({
-  blog
+  blog,
+  camp
 })
 
 export default createStore (rootReducer, composeWithDevTools(applyMiddleware(thunk)))
