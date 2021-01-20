27bfa7fbdc987a0b561c0f6930a3e45939395aef
Newland-A <ajn252@gmail.com>
2021-01-20 14:46:04 -0500

    adjust href link and debug map method

--- src/containers/CampgroundContainer.js
+++ src/containers/CampgroundContainer.js
@@ -10,10 +10,11 @@ class CampgroundContainer extends Component {
   }
 
   render() {
+    // debugger
     return (
-  
+        
         <div className='campgrounds'>
-          <h2><a href='/campgrounds'>Search CampGrounds </a></h2>
+          <h2> CampGrounds </h2>
           <div className='campground-intro'>
             <p>
             In my free time, I enjoy spending time with my family. Away from technology to reset my brain and connect with my loved ones. If your looking for a place to go maybe you would enjoy one of these places to spend time with your loved ones! 
@@ -24,25 +25,25 @@ class CampgroundContainer extends Component {
       <section className='campground-body'>
         <div className='display-campgrounds'>
           <ul className='camp-properties'>
-            <h2>Campgrounds: </h2>
-
-            {this.props.campground.campgrounds.map(campground => {
-
+            
+            
+          {this.props.camp.campgrounds?.map(campground => {
+              {/* debugger */}
               return (
                 
                 <div className='campground-link' key={campground.id}>
-                  <NavLink to={`/campground/${campground.id}`}>{campground.name}</NavLink>
-                  <li>{campground.img_url}</li>
-                  <li>{campground.avg_score}</li>
+                  <NavLink to={`/campgrounds/${campground.id}`}>{campground.name}</NavLink>
+                  {/* {/* <li>{campground.img_url}</li>
+                  <li>{campground.avg_score}</li> */}
                   <li>{campground.description}</li>
-                  <li>{campground.hiking}</li>
+                  {/* <li>{campground.hiking}</li>
                   <li>{campground.tent}</li>
                   <li>{campground.rv}</li>
                   <li>{campground.hammocks}</li>
-                  <li>{campground.price}</li>
+                  <li>{campground.price}</li> */} 
                 </div>
               )
-            })}
+            })} 
             </ul>
         </div>
       </section>
@@ -58,8 +59,8 @@ class CampgroundContainer extends Component {
 
 const mapStateToProps = state => {
   return({
-    campground: state.campground
+    camp: state.camp
   })
 }
 
-export default connect(mapStateToProps, {fetchCampgrounds})(CampgroundContainer);
+export default connect(mapStateToProps, { fetchCampgrounds })(CampgroundContainer);
