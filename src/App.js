a9092487fa9fde5c94629d4c19dd43a84bce3277
Newland-A <ajn252@gmail.com>
2021-01-22 10:57:52 -0500

    remove un-needed imports,  add route of technical projects

--- src/App.js
+++ src/App.js
@@ -2,16 +2,13 @@ import React from 'react'
 import {
   BrowserRouter as Router,
   Route
-} from 'react-router-dom'
+} from 'react-router-dom';
 import './App.css';
 import Home from './containers/Home';
 import BlogContainer from './containers/BlogContainer';
 import Blog from './components/blog-components/Blog';
 import CampgroundContainer from './containers/CampgroundContainer';
 import Campground from './components/campground-finder/Campground';
-// import Blog2 from './components/blog-components/Blog2';
-// import CampgroundContainer from './containers/CampgroundContainer'
-// import Campground from './components/campground-finder/Campground'
 
 function App() {
   return (
@@ -21,10 +18,11 @@ function App() {
           <Route exact path='/' component={Home} />
           <Route exact path='/blog' component={BlogContainer} />
           <Route exact path='/blog/:id' component={Blog} />
+          <Route exact path='/project'/>
+          <Route exact path='/project/:id' />
           <Route exact path='/campgrounds' component={CampgroundContainer} />
-          <Route exact path='/campgrounds/:id' component={Campground} />
-         {/* <Route exact path='/campground' component={CampgroundContainer} /> */}
-          {/* <Route exact path='/campground/:campgroundId' component={Campground} /> */}
+          <Route exact path='/campgrounds/:campgroundId' component={Campground} />
+      
         </div>
       </div>
     </Router>
