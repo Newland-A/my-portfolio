f5fb7b41b55ec6e6ce1fb5ad8893b9153ff71489
Newland-A <ajn252@gmail.com>
2021-01-22 11:00:34 -0500

    remove class component code and notes remove the find method and add the match parseInt for the id

--- src/components/blog-components/Blog.js
+++ src/components/blog-components/Blog.js
@@ -1,12 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, {  useEffect } from 'react';
 import CommentContainer from '../../containers/CommentContainer';
 import { useSelector } from 'react-redux';
 
  const Blog = ({ match }) => {
   const { postId } = match.params
 
+  const singlePost = parseInt(postId)
+
   const post = useSelector(state =>
-    state.posts.find(post => post.id === postId)
+    state.posts(singlePost)
     )
 
   useEffect(() => {
@@ -23,59 +25,19 @@ import { useSelector } from 'react-redux';
   }
   return (
     <section>
-    {/* // <div className="blog-container">
-    //     <div className="blog-content"> */}
       <article className="post">
+          <h1><a href='/blog'>Blog</a></h1>
           <h2 id="blog-title">{post.title}</h2>
-          {/* <div id="blog-body">{post.content}</div> */}
+    
           <p className="blog-content">{post.content}</p>
-        {/* // </div> */}
-        
-      {/* // </div> */}
+  
       </article>
-      <CommentContainer postId={this.state.postId} />
+      {/* <CommentContainer postId={this.state.postId} /> */}
     </section>
   )
 }
 
 export default Blog;
 
-// import React, { Component } from 'react';
-// import CommentContainer from '../../containers/CommentContainer';
 
-// export default class Blog extends Component {
-//   // const { blogId } = match.params
-
-//   state = {
-//     postId: parseInt(document.location.href.match(/\d+/g)),
-//     post: {}
-//   }
-
-//   componentDidMount() {
-//     fetch(`http://localhost:3000/posts/${this.state.postId}`)
-//     .then(resp => resp.json())
-//     .then(post => this.setState({
-//       ...this.state,
-//       post: post
-//     }))
-//   }
-
-//   componentDidUpdate(){
-
-//     document.getElementById('blog-title').innerHTML = this.state.post.title
-//     document.getElementById('blog-body').innerHTML = this.state.post.content
-//   }
-
-//   render() {
-//     return (
-//       <div className="blog-container">
-//         <div className="blog-content">
-//           <h2 id="blog-title">Loading Please Wait...</h2>
-//           <div id="blog-body"> </div>
-//         </div>
-//         <CommentContainer postId={this.state.postId} />
-//       </div>
-//     );
-//   }
-// }
 
