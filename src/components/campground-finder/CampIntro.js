3c967a126f82f2b0109c361c9947f1b8ede6c0f6
Newland-A <ajn252@gmail.com>
2021-01-22 11:06:24 -0500

    create camp intro component to break down camp container

---
+++ src/components/campground-finder/CampIntro.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class CampIntro extends Component {
+  render() {
+    return (
+      <div className='campground-intro'>
+            <p>
+            In my free time, I enjoy spending time with my family. Away from technology to reset my brain and connect with my loved ones. If your looking for a place to go maybe you would enjoy one of these places to spend time with your loved ones! 
+            </p>
+      </div>
+    );
+  }
+}
+
+export default CampIntro;
