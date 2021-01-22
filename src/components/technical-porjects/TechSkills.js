ef448c400200c7f07e3e3f767fc8c9c23f365e29
Newland-A <ajn252@gmail.com>
2021-01-22 11:08:39 -0500

    create folder of technical projects component

---
+++ src/components/technical-porjects/TechSkills.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class TechSkills extends Component {
+  render() {
+    return (
+      <div className="technical-project">
+            <h2>Technical Skills:</h2>
+            <p>
+              Ruby, Rails, MySql, Postgresql, JavaScript, React, OAuth, Git, Github, Bootstrap, Materialize, Semantic UI, Object-Oriented Programming (OOP), Behavior-Driven Development (BDD), Test-Driven Development (TDD), CRUD, HTML, CSS, JSON Web Tokens (JWT) and local storage, Devise, MVC backend structure, Adobe InDesign, Adobe Photoshop, Adobe Illustrator, Adobe Imageready, Adobe Flash, Adobe Dreamweaver, Microsoft Suite
+            </p>
+            <h2>Design Skills:</h2>
+            <p>
+            Bootstrap, Materialize, Semantic UI,
+            Drawing, Typography I & II, Design and Color Theory, Comprehensive Illustration, Electronic Drawing I & II, History of Graphic Design, Desktop Publishing, Electronic Layout and Design, Design and Presentation Development, Advertising and Art Portfolio, Multimedia and Animation I & II, Business Communications I & II
+            </p>
+          </div>
+    );
+  }
+}
+
+export default TechSkills;
