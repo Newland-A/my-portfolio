cf87519753b706ac1c28be163fd31789318335e8
Newland-A <ajn252@gmail.com>
2021-01-22 11:01:37 -0500

    change the description wording and import it to the container

--- src/components/blog-components/Description.js
+++ src/components/blog-components/Description.js
@@ -3,9 +3,11 @@ import React, { Component } from 'react';
 class Description extends Component {
   render() {
     return (
-      <div>
-        <p>Description about the blog</p>
-      </div>
+      <div className='blog-intro'>
+      <p>
+        Among the Components for my portfolio, I was required to write several blogs as a requirement for graduation at <a rel='noopener noreferrer' href="https://flatironschool.com" target="_blank">Flatiron School</a>. Post graduation my Blogs will be featured here. I will attempt to continue to add to my personal blogs here, in respect of my potential employer.
+      </p>
+    </div>
     );
   }
 }
