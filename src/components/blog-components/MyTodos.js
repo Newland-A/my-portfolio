13be9873de92e92d06fbae38be2abc4ae2b59c8c
Newland-A <ajn252@gmail.com>
2021-01-22 11:05:45 -0500

    create todo component and use to break down container component

---
+++ src/components/blog-components/MyTodos.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class MyTodos extends Component {
+  render() {
+    return (
+      <div>
+        <ul>
+            <h2>Things I'd Like To Achieve</h2>
+
+            <p>
+              My graduation day will be Feb. 11th. Afterwards, I will be continueing to grow my knowledge. On to the job hunt for that first right opportunity. 
+            </p>
+            <li>Gain an even deeper understanding of JavaScript, React.js and Node.js, anything JavaScript.</li>
+
+            <li>Share my knowledge with others to inspire them to always follow their dreams n matter how many side roads have to be taken to accomplish the end goal.</li>
+  
+            <li>I am excited to continue deeping my knowledge of Rails and JavaScript.</li>
+
+            <li>I look forward to building on to each of my projects. With new features</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default MyTodos;
