b62eb9f815beab8aeaae8f3eb90527d651f8f871
Newland-A <ajn252@gmail.com>
2021-01-27 04:20:56 -0500

    change class name

--- src/components/header/AboutMe.js
+++ src/components/header/AboutMe.js
@@ -4,7 +4,7 @@ class AboutMe extends Component {
   render() {
     return (
       <div>
-        <p className="subtitle">
+        <p className="about-me">
           Engineer experienced in Ruby on Rails and JavaScript / ReactJS based programming and a background in graphic design and leadership. Being a Stay-at-HomeSchool mom has given me considerable experience in problem-solving, leadership and making research-driven decisions. Being a mom has taught me with determination and persistence, anything can be accomplished. With me, you will be gaining a highly self-motivated and team-oriented person. I look forward to making connections and growing my knowledge with every new challenge.
         </p>
       </div>
