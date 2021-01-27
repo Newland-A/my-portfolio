d76a77060e6565982b7363f34a80b1efbe2d2a19
Newland-A <ajn252@gmail.com>
2021-01-27 04:17:33 -0500

    change from html tags to semantic ui-react components

--- src/components/blog-components/MyTodos.js
+++ src/components/blog-components/MyTodos.js
@@ -1,23 +1,24 @@
 import React, { Component } from 'react';
+import {List} from 'semantic-ui-react';
 
 class MyTodos extends Component {
   render() {
     return (
       <div>
-        <ul>
+        <List bulleted>
             <h2>Things I'd Like To Achieve</h2>
 
             <p>
               My graduation day will be Feb. 11th. Afterwards, I will be continueing to grow my knowledge. On to the job hunt for that first right opportunity. 
             </p>
-            <li>Gain an even deeper understanding of JavaScript, React.js and Node.js, anything JavaScript.</li>
+            <List.Item>Gain an even deeper understanding of JavaScript, React.js and Node.js, anything JavaScript.</List.Item>
 
-            <li>Share my knowledge with others to inspire them to always follow their dreams n matter how many side roads have to be taken to accomplish the end goal.</li>
+            <List.Item>Share my knowledge with others to inspire them to always follow their dreams n matter how many side roads have to be taken to accomplish the end goal.</List.Item>
   
-            <li>I am excited to continue deeping my knowledge of Rails and JavaScript.</li>
+            <List.Item>I am excited to continue deeping my knowledge of Rails and JavaScript.</List.Item>
 
-            <li>I look forward to building on to each of my projects. With new features</li>
-        </ul>
+            <List.Item>I look forward to building on to each of my projects. With new features</List.Item>
+        </List>
       </div>
     );
   }
