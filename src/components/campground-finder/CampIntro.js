cfae53df96d4180894ca55715622351782348912
Newland-A <ajn252@gmail.com>
2021-01-27 04:18:23 -0500

    change from html tags to semantic ui-react components

--- src/components/campground-finder/CampIntro.js
+++ src/components/campground-finder/CampIntro.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-
+import {Container} from 'semantic-ui-react';
 class CampIntro extends Component {
   render() {
     return (
-      <div className='campground-intro'>
+      <Container width="550px">
             <p>
             In my free time, I enjoy spending time with my family. Away from technology to reset my brain and connect with my loved ones. If your looking for a place to go maybe you would enjoy one of these places to spend time with your loved ones! 
             </p>
-      </div>
+      </Container>
     );
   }
 }
