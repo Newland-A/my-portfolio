ac538dc7adc7d0bdcb48d1afd4655c22159b668e
Newland-A <ajn252@gmail.com>
2021-01-27 04:15:16 -0500

    change from html tags to semantic ui-react components changed naming conventions import comments components

--- src/components/blog-components/Blog.js
+++ src/components/blog-components/Blog.js
@@ -1,39 +1,55 @@
-import React, {  useEffect } from 'react';
+import React from 'react';
 import CommentContainer from '../../containers/CommentContainer';
 import { useSelector } from 'react-redux';
+import {Container, Segment} from 'semantic-ui-react';
+import NavBar from '../NavBar';
+import { Feed } from 'semantic-ui-react'
 
  const Blog = ({ match }) => {
+
   const { postId } = match.params
 
   const singlePost = parseInt(postId)
 
-  const post = useSelector(state =>
-    state.posts(singlePost)
-    )
+  const posts = useSelector(state => state.blog.posts[singlePost])
 
-  useEffect(() => {
-    document.title = post.tile;
-    document.content = post.content;
-  })
 
-  if (!post) {
+
+  if (!posts) {
     return(
-      <section>
-        <h2>Post not found!</h2>
-      </section>
+      <div>
+        <NavBar />
+        <Container>
+          <h2>Post not found!</h2>
+        </Container>
+      </div>
     )
   }
   return (
-    <section>
-      <article className="post">
-          <h1><a href='/blog'>Blog</a></h1>
-          <h2 id="blog-title">{post.title}</h2>
-    
-          <p className="blog-content">{post.content}</p>
-  
-      </article>
-      {/* <CommentContainer postId={this.state.postId} /> */}
-    </section>
+    <div>
+      <NavBar />
+      
+      <Container>
+        <Segment raised >
+
+            <h1><a href='/blog'>Blog</a></h1>
+            <h2 id="blog-title">{posts.title}</h2>
+            <div className="blog-content">
+              <p className="blog-content">{posts.content}</p>
+            </div>
+
+            <br />
+        
+        </Segment>
+      </Container>
+      <br /><br />
+      <Container>
+        <Segment raised>
+        <p>Please leave Comments, Suggestions, Advice and Encouraging Criticism Welcomed!</p>
+          <CommentContainer postId={postId} />
+        </Segment>
+      </Container>
+    </div>
   )
 }
 
