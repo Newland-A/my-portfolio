191fc555e575722a754fbd2ec5cd3c7afd1b575d
Newland-A <ajn252@gmail.com>
2021-01-27 04:27:18 -0500

    change from html tags to semantic react components, add styling to components

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -4,35 +4,46 @@ import Myname from '../components/header/Myname';
 import {NavLink} from 'react-router-dom'
 import CampDescription from '../components/campground-finder/CampDescription';
 import Description from '../components/blog-components/Description'
-import TechSkills from '../components/technical-porjects/TechSkills';
+import TechDescription from '../components/technical-porjects/TechDescription'
 import NavBar from '../components/NavBar';
-
+import { Container, Segment, Grid } from 'semantic-ui-react'
+// import 
 export default class Home extends Component {
   render() {
     return (
-      <div className='Home'>
+      <div>
+      
       <NavBar />
-        <section className='headline'>
-          <Myname />
-          <AboutMe />
-        </section>
+      <Grid columns='equal'>
+       
+        <Grid.Column floated="left">
+          <Segment raised>
+            <Container className="home_intro">
+              <Myname />
+              <AboutMe />
+            </Container>
 
-        <section className='projects-blogs'>
-          <div className='project blog'>
-            <h2><NavLink to='/blog'>Blog</NavLink></h2>
-            <Description />
-          </div>
-        </section>
+            <Container className="projects_home">
+              <h2><NavLink to='technical-projects'>Projects</NavLink></h2>
+                <TechDescription />
+            </Container>
+              
+            <Container className="campgrounds_home">
+              <h2><NavLink to='/campgrounds'>Campgrounds</NavLink></h2>
+              <CampDescription />
+            </Container>
+          </Segment>
+        </Grid.Column>
 
-        <section className="tech-propjects">
-        <h2><NavLink to='technical-projects'>Projects</NavLink></h2>
-          <TechSkills />
-        </section>
-
-        <section className="project-campgrounds">
-          <h2><NavLink to='/campgrounds'>Campgrounds</NavLink></h2>
-          <CampDescription />
-        </section>
+        <Grid.Column floated='right' width={5}>
+          <Container className="blogs_home">
+              <Segment raised className="blogs_home">
+                <h2><NavLink to='/blog'>Blog</NavLink></h2>
+                <Description />
+              </Segment>
+          </Container>
+        </Grid.Column>
+      </Grid>
       </div>
 
     );
