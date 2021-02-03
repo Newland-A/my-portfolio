b9e69a51bd184e52a01e8e6576d90cbf6adff729
Newland-A <ajn252@gmail.com>
2021-02-03 00:56:22 -0500

    add styling to button semantic react component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,12 +5,12 @@ import { Button, Segment } from 'semantic-ui-react'
 const NavBar = () => {
   return (
     <div className='navbar'>
-      <Segment inverted>
-        <Button basic inverted color="black"><NavLink to='/' className=''>Home</NavLink> </Button>
+      <Segment inverted centered>
+        <Button basic  inverted color="black"><NavLink to='/' className=''>Home</NavLink> </Button>
    
-        <Button basic inverted color="purple"><NavLink to='/blog' className=''>Blog</NavLink> </Button>  
+        <Button basic  inverted color="purple"><NavLink to='/blog' className=''>Blog</NavLink> </Button>  
      
-        <Button basic inverted color="dark blue"><NavLink to='/technical-projects' className=''>Projects</NavLink></Button>
+        <Button basic  inverted color="dark blue"><NavLink to='/technical-projects' className=''>Projects</NavLink></Button>
       
         <Button basic inverted color="black"><NavLink to='/campgrounds' className=''>CampGrounds</NavLink></Button>
       </Segment>
