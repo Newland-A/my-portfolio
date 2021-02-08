d13d8342f42873df1a550de5b4917cd411d516ea
Newland-A <ajn252@gmail.com>
2021-02-07 19:43:17 -0500

    adjust layout of file

--- src/components/campground-finder/CampIntro.js
+++ src/components/campground-finder/CampIntro.js
@@ -3,8 +3,8 @@ import {Container} from 'semantic-ui-react';
 class CampIntro extends Component {
   render() {
     return (
-      <Container width="550px">
-            <p>
+      <Container width="550px">         
+       <p>
             In my free time, I enjoy spending time with my family. Away from technology to reset my brain and connect with my loved ones. If your looking for a place to go maybe you would enjoy one of these places to spend time with your loved ones! 
             </p>
       </Container>
