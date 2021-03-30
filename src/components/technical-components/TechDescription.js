f2c22d5303d23a18c1650d14fb5dad1722d88949
Newland-A <ajn252@gmail.com>
2021-03-30 09:26:33 -0400

    change directory name

---
+++ src/components/technical-components/TechDescription.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Description extends Component {
+  render() {
+    return (
+      <div>
+        <p>The projects below all focus on different features, of my studies in researching and learning code. They all have room for improvement as with any application. So of the projects focus on authentication, Others focus on the front end of the appliation utilizing Bootstrap, Materialize, and the portfoilio itself uses a Semantic ReactUI framework. All of the projects below utilize a RESTful API.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Description;
