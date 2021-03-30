36d1167cae003e1ee51886ae8103d16b4b21d04d
Newland-A <ajn252@gmail.com>
2021-03-30 09:25:40 -0400

    correct grammer error

--- src/components/blog-components/MyTodos.js
+++ src/components/blog-components/MyTodos.js
@@ -13,11 +13,11 @@ class MyTodos extends Component {
             </p>
             <List.Item>Gain an even deeper understanding of JavaScript, React.js and Node.js, anything JavaScript.</List.Item>
 
-            <List.Item>Share my knowledge with others to inspire them to always follow their dreams n matter how many side roads have to be taken to accomplish the end goal.</List.Item>
+            <List.Item>Share my knowledge with others to inspire them to always follow their dreams matter how many side roads have to be taken to accomplish the end goal.</List.Item>
   
             <List.Item>I am excited to continue deeping my knowledge of Rails and JavaScript.</List.Item>
 
-            <List.Item>I look forward to building on to each of my projects. With new features</List.Item>
+            <List.Item>I look forward to building on to each of my projects. With new features.</List.Item>
         </List>
       </div>
     );
